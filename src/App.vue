fa0f4385b61547b8aa3e5e3f652b44eaf74d5e12
William <lisirui086@gmail.com>
2023-05-10 22:27:53 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div class="canvas" ref='sceneDom'></div>
+</template>
+
+<style scoped>
+  .canvas {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
