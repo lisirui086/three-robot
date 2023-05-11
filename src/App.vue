198529cba9f34b37c1b027c4f4d3526164418096
William <lisirui086@gmail.com>
2023-05-11 15:20:48 +0800

    导入glb模型并解压，导入镜面反射库，实现镜像效果

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,13 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
 
 // 引入GLTF加载器
-// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 // 用Draco库压缩的几何装载器。
-// import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
+
+// Reflector 镜面反射效果
+import { Reflector } from 'three/addons/objects/Reflector.js'
 
 // 画布节点
 const sceneDom = ref(null)
@@ -51,9 +54,73 @@ function handResize () {
   renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
+// 实例化GLTF加载器（压缩、DRACO加载器（解压
+const gltfLoader = new GLTFLoader()
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/gltf/')
+dracoLoader.setDecoderConfig({ type: 'js' })
+gltfLoader.setDRACOLoader( dracoLoader )
+gltfLoader.load('/assets/robot.glb', gltf => {
+  const robot = gltf.scene
+  scene.add(robot)
+})
+
+// 添加平行光
+const light1 = new THREE.DirectionalLight( 0xffffff, 1 )
+light1.position.set(0, 10, 10)
+
+const light2 = new THREE.DirectionalLight( 0xffffff, 1 )
+light2.position.set(0, 10, -10)
+
+const light3 = new THREE.DirectionalLight( 0xffffff, 1 )
+light3.position.set(10, 10, 10)
+
+scene.add(light1, light2, light3)
+
+
+
 onMounted(() => {
   // 将场景添加到画布上
   sceneDom.value.appendChild( renderer.domElement )
+
+  // 创建视频
+  const video = document.createElement('video')
+  video.src = '/assets/zp2.mp4'
+  // 媒介文件完成播放后再次开始播放
+  video.loop = true
+  // 视频的音频输出为静音。
+  video.muted = true
+  video.play()
+
+  // 创建光圈
+  const videoTexture = new THREE.VideoTexture(video)
+  const videoGeometry = new THREE.PlaneGeometry(16, 9)
+  const videoMaterial = new THREE.MeshBasicMaterial({
+    map: videoTexture,
+    // 此材质是否透明
+    transparent: true,
+    // 定义将要渲染哪一面 - 正面，背面或两者
+    side: THREE.DoubleSide,
+    opacity: 1.0,
+    // alpha贴图是一张灰度纹理，用于控制整个表面的不透明度。
+    // （黑色：完全透明；白色：完全不透明）。 默认值为null
+    alphaMap: videoTexture
+  })
+  const videoMesh = new THREE.Mesh(videoGeometry, videoMaterial)
+  videoMesh.position.set(0, 0.2, 0)
+  videoMesh.rotation.x = -Math.PI/2
+  scene.add(videoMesh)
+
+  // 创建镜面反射效果
+  
+  const reflectorPlane = new Reflector(videoGeometry, {
+    textureWidth: window.innerWidth,
+    textureHeight: window.innerHeight,
+    color: 0x333333,
+    recursion: 1
+  })
+  reflectorPlane.rotation.x = -Math.PI / 2
+  scene.add( reflectorPlane )
 
   // 创建轨道控制器
   const controls = new OrbitControls( camera, sceneDom.value )
